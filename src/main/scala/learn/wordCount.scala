6c4e501af6fae18615efd91566af76a199c24a9d
sx0404 <s1264242822@163.com>
2016-05-25 16:18:40 +0800

    test1

---
+++ src/main/scala/learn/wordCount.scala
@@ -0,0 +1,57 @@
+package learn
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by xinmei on 16/5/25.
+  */
+object wordCount {
+
+
+  def main(args:Array[String])={
+
+
+    val conf = new SparkConf()
+
+    val sc  = new SparkContext(conf)
+
+
+
+
+    val hdfspath = "hdfs:///lxw/test"
+
+
+    val hadoopConf = sc.hadoopConfiguration
+
+
+    val awsAccessKeyId = args(0)
+    val awsSecretAccessKey = args(1)
+
+    val anchordate = "20160426"
+
+    hadoopConf.set("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+
+    hadoopConf.set("fs.s3n.awsAccessKeyId", awsAccessKeyId)
+
+    hadoopConf.set("fs.s3n.awsSecretAccessKey", awsSecretAccessKey)
+
+
+    val text = sc.textFile(hdfspath)
+      .flatMap{case line  =>
+        val item  = line.replaceAll(" +", " ").split("\\W+").map(y => (y, 1))
+        item
+      }
+      .reduceByKey((a:Int, b:Int) => a + b)
+      .collect()
+      .foreach(x => println("lixuefei log " + x))
+
+
+
+    sc.stop()
+
+  }
+
+
+
+
+}
